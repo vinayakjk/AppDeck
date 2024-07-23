c8277f7a0e08f1fc0d5af00f18a2430dffaae03f
vinayakjk <vinayak.jalanakar@gmail.com>
2024-07-23 12:10:07 +0530

    Initial Commit

---
+++ MFCApplication2/MFCApplication2Dlg.h
@@ -0,0 +1,36 @@
+
+// MFCApplication2Dlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCApplication2Dlg dialog
+class CMFCApplication2Dlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCApplication2Dlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CButton Close;
+};
---
+++ MFCApplication2/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MFCApplication2.rc
+//
+#define IDCANCEL2                       3
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDI_ICON1                       101
+#define IDD_MFCAPPLICATION2_DIALOG      102
+#define IDI_ICON2                       102
+#define IDI_ICON3                       103
+#define IDI_ICON4                       104
+#define IDR_MAINFRAME                   128
+#define IDD_DEVICE_INFO                 130
+#define IDD_UWF_MGR                     132
+#define IDD_AUTOLOGON_MGR               133
+#define IDD_DIALOG1                     136
+#define IDD_USB_MGR                     136
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
